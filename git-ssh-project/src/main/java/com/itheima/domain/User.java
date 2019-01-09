1cc5e175bb232471277e12e93cfd74a11ca792ef
VenkoXie <495361752@qq.com>
2019-01-09 11:07:07 +0800

    123

---
+++ git-ssh-project/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,13 @@
+package com.itheima.domain;
+
+/**
+ * @Author: VenkoXie
+ * @Date: 2019/1/9 11:05
+ * @Version 1.0
+ * @Description:
+ */
+public class User {
+    private String username;
+    private String password;
+    private Integer age;
+}
