1a2b346765bcde05178162bdfda1e3ef54b643ea
VenkoXie <495361752@qq.com>
2019-01-09 11:25:27 +0800

    123

---
+++ git-ssh-project/src/main/java/com/itheima/domain/People.java
@@ -0,0 +1,12 @@
+package com.itheima.domain;
+
+/**
+ * @Author: VenkoXie
+ * @Date: 2019/1/9 11:24
+ * @Version 1.0
+ * @Description:
+ */
+public class People {
+    private String username;
+    private String password;
+}
